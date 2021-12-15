17664fcdd74c4d5c5e2cbdc4119e0de5f0cd263a
ShrustiC <chheda.sh@northeastern.edu>
2021-12-15 15:58:34 -0500

    final exam code

---
+++ src/main/java/edu/neu/coe/info6205/Pell.java
@@ -0,0 +1,43 @@
+package edu.neu.coe.info6205;
+
+
+
+public class Pell {
+    public Pell() {
+
+    }
+
+    public long get(int n) {
+        if (n < 0) throw new UnsupportedOperationException("Pell.get is not supported for negative n");
+        if (n<=2){
+            return n;
+
+        }
+        long i = 1;
+        long j = 2;
+        long r = 0;
+        for (int l = 3; l<=n;l++){
+            r = 2 * j+i;
+            i=j;
+            j=r;
+        }
+        return r;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
